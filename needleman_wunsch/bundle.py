557d409d225fd7d3e07139a633d9bd3721c01678
jeannebourgarel <jeanne.bourgarel@mines-paristech.fr>
2020-03-31 10:05:39 +0200

    Update bundle.py
    après autopep8

--- needleman_wunsch/bundle.py
+++ needleman_wunsch/bundle.py
@@ -1,16 +1,16 @@
 from argparse import ArgumentParser
 from ruler import Ruler
 parser = ArgumentParser()
-parser.add_argument("fichier", help = "le nom du fichier")
+parser.add_argument("fichier", help="le nom du fichier")
 args = parser.parse_args()
 print(args.fichier)
 
-with open(args.fichier, 'r') as f :
+with open(args.fichier, 'r') as f:
     L = []
-    for line in f :
+    for line in f:
         L.append(line.strip())
     n = len(L)
-for k in range(0, n - 1, 2) :
+for k in range(0, n - 1, 2):
     ruler = Ruler(L[k], L[k + 1])
     ruler.compute()
     print(f"=== exemple #{k//2 + 1} - distance = {ruler.distance()}")
